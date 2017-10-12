9482bdba837802180472d3bc754474f3c816f8cc
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-10-12 10:00:56 +0300

    anketa baigta

--- script2.js
+++ script2.js
@@ -9,3 +9,36 @@ function im() {
 }
 
 cb_pw = document.getElementById("account");
+
+cb_pw.addEventListener("change", function(){
+	if (cb_pw.checked) {
+		document.getElementById("pw").style.display = "block";
+		document.getElementById("pwr").style.display = "block";
+	} else {
+		document.getElementById("pw").style.display = "none";
+		document.getElementById("pwr").style.display = "none";
+	}
+})
+
+function namai() {
+	document.getElementById("pristatymas").style.display = "none";
+}
+
+function kitu() {
+	document.getElementById("pristatymas").style.display = "block";
+}
+
+// rb_pristn = document.getElementById("namo");
+// rb_pristk = document.getElementById("kitur");
+
+// rb_pristn.addEventListener("change", function(){
+// 	if (rb_pristn.checked) {
+// 		document.getElementById("pristatymas").style.display = "block";
+// 	} else {
+// 		document.getElementById("pristatymas").style.display = "none";
+// 	}
+
+// 	if (rb_pristk.checked) { 
+// 		document.getElementById("pristatymas").style.display = "none";
+// 	}	
+// })
